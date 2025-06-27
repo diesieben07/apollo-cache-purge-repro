5d83d8970936b0e6fcc327970405efea52324600
Take Weiland <diesieben07@gmail.com>
2025-06-27 12:22:04 +0200

    Initial Commit

---
+++ demo.ts
@@ -0,0 +1,46 @@
+import {ApolloClient, ApolloLink, type FetchResult, gql, InMemoryCache} from '@apollo/client/core';
+import type {NextLink, Operation} from '@apollo/client/link/core/types';
+import {Observable} from "zen-observable-ts";
+
+/**
+ * Set to true to observe that it works fine when returnPartialData is enabled
+ */
+const returnPartialData = false;
+/**
+ * Set to true to make the "server" return different data every time, demonstrating that this also
+ * does not observe the bug
+ */
+const returnDifferentData = false;
+
+let index = 0;
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new ApolloLink((operation: Operation, forward: NextLink) => {
+    return new Observable(observer => {
+      const timeout = setTimeout(() => {
+        observer.next({
+          data: {
+            message: "Hello World" + (returnDifferentData ? (index++) : ""),
+          }
+        } satisfies FetchResult);
+        observer.complete();
+      }, 500);
+      return () => clearTimeout(timeout);
+    });
+  }),
+});
+
+client.watchQuery({
+    query: gql`
+      query GetMessage {
+        message
+      }
+    `,
+  notifyOnNetworkStatusChange: true,
+  returnPartialData,
+}).subscribe({
+  next: v => console.log('data', v)
+});
+
+setTimeout(() => client.cache.reset(), 1000);
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Demonstrator for Apollo Issue
+
+To run:
+- `npm i`
+- `npx tsx demo.ts`
+
+Observe that in default configuration the observable does not emit again after the last "loading: true" notification.
